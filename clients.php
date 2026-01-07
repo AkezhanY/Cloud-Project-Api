d1540ff0f62fe2656a3707851e92813683ee0313
Akezhan <168573415+AkezhanY@users.noreply.github.com>
2026-01-07 11:43:17 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -1,3 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+
+// Остальной код...
+?>
 CREATE TABLE clients (
     id SERIAL PRIMARY KEY,
     type VARCHAR(50),
@@ -13,4 +21,5 @@ INSERT INTO clients (type, count) VALUES
 ('Partners', 30),
 ('Resellers', 40),
 ('Distributors', 25),
+
 ('Government', 35);
