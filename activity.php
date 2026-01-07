f63afe16a48f835fea3d1f3bdcebe19212aaef14
Akezhan <168573415+AkezhanY@users.noreply.github.com>
2026-01-07 23:17:20 +0100

    Update activity.php

--- activity.php
+++ activity.php
@@ -1,21 +1,23 @@
-
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 
-// Остальной код...
+$data = [
+    ["hour" => 9, "users" => 120],
+    ["hour" => 10, "users" => 250],
+    ["hour" => 11, "users" => 310],
+    ["hour" => 12, "users" => 280],
+    ["hour" => 13, "users" => 200],
+    ["hour" => 14, "users" => 270],
+    ["hour" => 15, "users" => 320],
+    ["hour" => 16, "users" => 300],
+    ["hour" => 17, "users" => 350],
+    ["hour" => 18, "users" => 400],
+    ["hour" => 19, "users" => 450],
+    ["hour" => 20, "users" => 500]
+];
+
+echo json_encode($data);
 ?>
-CREATE TABLE hourly_activity (
-    id SERIAL PRIMARY KEY,
-    hour INTEGER,
-    users INTEGER
-);
-
--- Inserting data into hourly_activity
-INSERT INTO hourly_activity (hour, users) VALUES
-(9, 120), (10, 250), (11, 310), (12, 280),
-(13, 200), (14, 270), (15, 320), (16, 300),
-
-(17, 350), (18, 400), (19, 450), (20, 500);
