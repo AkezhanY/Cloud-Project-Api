22c18a14b31daea14559d1e5a2836df2644625da
Akezhan <168573415+AkezhanY@users.noreply.github.com>
2026-01-07 23:18:04 +0100

    Refactor clients.php to return JSON data
    Replaced SQL table creation and insertion with PHP array and JSON response.

--- clients.php
+++ clients.php
@@ -4,22 +4,16 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 
-// Остальной код...
+$data = [
+    ["type" => "Regular", "count" => 150],
+    ["type" => "VIP", "count" => 45],
+    ["type" => "New", "count" => 80],
+    ["type" => "Companies", "count" => 60],
+    ["type" => "Partners", "count" => 30],
+    ["type" => "Resellers", "count" => 40],
+    ["type" => "Distributors", "count" => 25],
+    ["type" => "Government", "count" => 35]
+];
+
+echo json_encode($data);
 ?>
-CREATE TABLE clients (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(50),
-    count INTEGER
-);
-
--- Inserting data into clients
-INSERT INTO clients (type, count) VALUES
-('Regular', 150),
-('VIP', 45),
-('New', 80),
-('Companies', 60),
-('Partners', 30),
-('Resellers', 40),
-('Distributors', 25),
-
-('Government', 35);
